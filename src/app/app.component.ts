d23afc71295280ceb7f792f84dd73490fc47d013
siva <siva.cs579@gmail.com>
2022-08-09 16:28:16 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
+import { Version } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { UserIdleService } from 'angular-user-idle';
+import { CountdownConfig, CountdownEvent } from 'ngx-countdown';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'practice';
+  leftTime!: any;
+  constructor(private userIdle: UserIdleService) {}
+  ngOnInit(): void {
+    //Start watching for user inactivity.
+    this.userIdle.startWatching();
+
+    // Start watching when user idle is starting.
+    this.userIdle.onTimerStart().subscribe((count) => {
+      this.leftTime = count;
+      console.log(count);
+    });
+
+    // Start watch when time is up.
+    this.userIdle.onTimeout().subscribe(() => {
+      console.log('Time is up!');
+      // this.restart();
+      this.stop();
+      this.stopWatching();
+    });
+  }
+
+  stop() {
+    this.userIdle.stopTimer();
+  }
+
+  stopWatching() {
+    this.userIdle.stopWatching();
+  }
+
+  startWatching() {
+    this.userIdle.startWatching();
+  }
+
+  restart() {
+    this.userIdle.resetTimer();
+  }
+
+  config: CountdownConfig = {
+    leftTime: 60,
+    format: 'mm:ss',
+    prettyText: (text) => {
+      return text
+        .split(':')
+        .map((v) => `<span class="item">${v}</span>`)
+        .join('');
+    },
+  };
+
+  handleEvent(e: CountdownEvent) {
+    console.log('Actions', e);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { UserIdleModule } from 'angular-user-idle';
+import { CountdownModule } from 'ngx-countdown';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CountdownModule,
+    UserIdleModule.forRoot({idle: 10, timeout: 20, ping: 20})
   ],
   providers: [],
   bootstrap: [AppComponent]
